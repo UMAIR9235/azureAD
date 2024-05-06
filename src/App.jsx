65e4adf961eb4ef7206d64771dd6f24c7ea5738c
UMAIR9235 <umairslm80@gmail.com>
2024-05-06 14:47:18 +0530

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState();
+  const fetchData = async () => {
+    try {
+      console.log("calling");
+      const response = await fetch(
+        "https://localhost:7185/WeatherForecast/callback",
+        {
+          method: "POST",
+          headers: {
+            accept: "*/*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code: code,
+          }),
+        }
+      );
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error, "oh noooooooooooooooooooo");
+    }
+  };
   return (
-    <div className="flex items-center justify-center h-dvh bg-blue-400">
-      <h1>Azure Ad Mugaiz</h1>
+    <div className="flex flex-col items-center justify-center h-dvh bg-blue-400 gap-10">
+      <h1 className="text-4xl text-red-500 font-bold">Azure Ad Mugaiz</h1>
+      <div className="flex flex-col gap-3">
+        <label className="text-white" htmlFor="codeInput">
+          Copy above code and paste it into input field.
+        </label>
+        <input
+          id="codeInput"
+          type="text"
+          className="rounded-lg py-2 text-lg px-2"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <button onClick={() => fetchData()} className="border p-3 rounded-lg">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
