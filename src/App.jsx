ae9dd253157049e2b385622ef75a84dd494dd4ce
UMAIR9235 <umairslm80@gmail.com>
2024-05-06 14:13:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-dvh bg-blue-400">
+      <h1>Azure Ad Mugaiz</h1>
+    </div>
+  );
+}
+
+export default App;
